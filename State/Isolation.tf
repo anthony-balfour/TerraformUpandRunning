ab51b82be8e4952bfd4d6a124acd9a1936480297
antb4 <ant5692@gmail.com>
2023-12-31 19:36:40 -0800

    component isolation example

--- State/Isolation.tf
+++ State/Isolation.tf
@@ -13,12 +13,22 @@
 #### Isolation via File Layout
 
 - config files for each environment into separate folders.
-  -staging files in stage
-  -production files in prod
+  -staging files in "stage"
+  -production files in "prod"
 
   ## different backend for each envrionment, using diff access controls
       -so separate aws account with separate s3 bucket
  */
+
+ # Component Isolation
+
+/**
+
+- Isolating VPC, services, databases
+- For example: If I set up a VPC component that changed only every couple months,
+- and a web server that changed multiple times per day, it would be best to separate
+- the infrastructure configurations of both
+*/
 
 
 
